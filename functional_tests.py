7ac7f4bfda0e04ab19ba9201f91c59f5e5c09071
Matheus Macedo <mcmacedoprog@outlook.com>
2016-10-17 21:43:42 -0200

    Primeiro TF utilizando unittest.

--- functional_tests.py
+++ functional_tests.py
@@ -1,8 +1,49 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
+import unittest
 
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+
+        # Edith ouviu falar sobre um novo app online muito legal.
+        # Então ela foi dar uma olhada na homepage.
+        self.browser.get('http://localhost:8000')
+
+        # Ela notou que a página tem To-Do escrito na título e em seu
+        # cabeçalho.
+        self.assertIn("To-Do", self.browser.title)
+        self.fail("Finish the test!")
+
+        # Ela então é convidada a adicionar um item a lista de To-Do.
+
+        # Ela digitou "Buy peacock feathers" na text box.
+
+        # Quando ela aperta enter, a página atualiza, e agora a página
+        # lista. #1: Buy peacock feather como uma item da lista de
+        # To-Do's.
+
+        # Ainda existe um text box sugerindo a ela que adicione outro
+        # item. Ela então digita "Use peacock feathers to make a fly".
+
+        # A página atualiza de novo, e agora ela tem dois items em sua
+        # lista.
+
+        # Edith se pergunta se o site vai se lembrar de sua lista.
+        # Então ela nota que o site gerou uma URL única para ela.
+        # -- Isto é mais um texto explicativo de um efeito.
+
+        # Ela visita aquela URL - sua lista de To-do's continua lá.
+
+        # Satisfeita, ela vai dormir.
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
