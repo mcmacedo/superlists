1903dc1d5632640856043210c51bc230b303d61c
Matheus Macedo <mcmacedoprog@outlook.com>
2016-10-23 14:31:02 -0200

    View básica retorna o mínimo de HTML.

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def home_page(request):
-    pass
+    html = '<html><title>To-Do lists</title></html>'
+    return HttpResponse(html)
