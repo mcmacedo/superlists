3e0fad044fc21f2e61eb1baf509b346c44bde323
Matheus Macedo <mcmacedoprog@outlook.com>
2016-10-23 16:03:38 -0200

    Template criado e app adicionado ao settings.py.

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home_page(request):
-    html = '<html><title>To-Do lists</title></html>'
-    return HttpResponse(html)
+    return render(request, 'home.html')
