918996ef1f1290bd86297d70f18f20bdfc22064b
Matheus Macedo <mcmacedoprog@outlook.com>
2016-10-23 18:03:50 -0200

    Teste funcional refatorado.

--- functional_tests.py
+++ functional_tests.py
@@ -13,6 +13,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
 
         # Edith ouviu falar sobre um novo app online muito legal.
@@ -39,11 +44,7 @@ class NewVisitorTest(unittest.TestCase):
         # lista. #1: Buy peacock feather como uma item da lista de
         # To-Do's.
         inputbox.send_keys(Keys.ENTER)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # Ainda existe um text box sugerindo a ela que adicione outro
         # item. Ela então digita "Use peacock feathers to make a fly".
@@ -53,11 +54,9 @@ class NewVisitorTest(unittest.TestCase):
 
         # A página atualiza de novo, e agora ela tem dois items em sua
         # lista.
-        table = self.browser.find_element_by_id('id_list_table')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn(
-            '2: Use peacock feathers to make a fly',
-            [row.text for row in rows]
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table(
+            '2: Use peacock feathers to make a fly'
         )
 
         # Edith se pergunta se o site vai se lembrar de sua lista.
