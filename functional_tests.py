9925ccd1b6ec6451df1b4fdfcd5f44cc825a4caf
Matheus Macedo <mcmacedoprog@outlook.com>
2016-10-23 15:49:22 -0200

    Teste Funcional agora checa se podemos adicionar um to-do item.

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -21,18 +22,34 @@ class NewVisitorTest(unittest.TestCase):
         # Ela notou que a página tem To-Do escrito na título e em seu
         # cabeçalho.
         self.assertIn("To-Do", self.browser.title)
-        self.fail("Finish the test!")
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # Ela então é convidada a adicionar um item a lista de To-Do.
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # Ela digitou "Buy peacock feathers" na text box.
+        inputbox.send_keys('Buy peacock feathers')
 
         # Quando ela aperta enter, a página atualiza, e agora a página
         # lista. #1: Buy peacock feather como uma item da lista de
         # To-Do's.
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
 
         # Ainda existe um text box sugerindo a ela que adicione outro
         # item. Ela então digita "Use peacock feathers to make a fly".
+        self.fail("Finish the test!")
 
         # A página atualiza de novo, e agora ela tem dois items em sua
         # lista.
