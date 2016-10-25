d7b210a0aa309c6f72855a36459828f1dcf0e6f6
Matheus Macedo <mcmacedoprog@outlook.com>
2016-10-25 21:03:13 -0200

    Novos testes funcionais adicionados.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -40,10 +40,12 @@ class NewVisitorTest(LiveServerTestCase):
         # Ela digitou "Buy peacock feathers" na text box.
         inputbox.send_keys('Buy peacock feathers')
 
-        # Quando ela aperta enter, a página atualiza, e agora a página
-        # lista. #1: Buy peacock feather como uma item da lista de
-        # To-Do's.
+        # Quando ela aperta enter, ela é levada a uma nova url, e
+        # agora a página lista "1: Buy peacock feathers" como um item
+        # em uma tabela de To-Do's.
         inputbox.send_keys(Keys.ENTER)
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # Ainda existe um text box sugerindo a ela que adicione outro
@@ -54,16 +56,47 @@ class NewVisitorTest(LiveServerTestCase):
 
         # A página atualiza de novo, e agora ela tem dois items em sua
         # lista.
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table(
             '2: Use peacock feathers to make a fly'
         )
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+
+        # Agora um novo usuário, Francis, acessa o site.
+
+        # Nós utilizaremos uma nova sessão do navegador para termos
+        # certeza de que nenhuma informação de Edith esteja presente
+        # mesmo nos cookies do site.
+        self.browser.quit()
+        self.browser = webdriver.Chrome()
+
+        # Francis visita a página e não há qualquer sinhal da lista de
+        # Edith.
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('Use peacock feathers to make a fly', page_text)
+
+        # Francis inicia uma nova lista adicionando um novo item. Ele
+        # é menos interessante que Edith.
+        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box.send_keys("Buy milk")
+        input_box.send_keys(Keys.ENTER)
+
+        # Francis vai para sua url única.
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # Novamente, não há sinal da lista de Edith.
+        page_text = self.broser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
 
         # Edith se pergunta se o site vai se lembrar de sua lista.
         # Então ela nota que o site gerou uma URL única para ela.
         # -- Isto é mais um texto explicativo de um efeito.
-        self.fail("Finish the test!")
 
         # Ela visita aquela URL - sua lista de To-do's continua lá.
 
-        # Satisfeita, ela vai dormir.
+        # Satisfeitos, os dois foram dormir.
+        self.fail("Finish the test!")
